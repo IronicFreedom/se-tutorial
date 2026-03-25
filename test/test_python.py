b700010b847f18f149406daf9774cd689e2b6048
Thurn, Johann <johann.thurn@dlr.de>
2026-03-25 14:35:46 +0100

    first successful test

--- test/test_python.py
+++ test/test_python.py
@@ -1,2 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from src.vicsek_pkg_example.test2 import VicsekModel
+
 def test_main():
     assert(True)
+
+def test_distance_type(r=np.random.rand(3,2)):
+    assert(type(VicsekModel.distances(r, 2))==type(np.array(1)))
