944c78708d6f5f5543a874fc05ec7a138aa6ba01
Thurn, Johann <johann.thurn@dlr.de>
2026-04-14 10:56:01 +0200

    updated toml for pixi

--- test/test_python.py
+++ test/test_python.py
@@ -1,9 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from src.vicsek_pkg_example.test2 import VicsekModel
 
 def test_main():
     assert(True)
 
 def test_distance_type(r=np.random.rand(3,2)):
-    assert(type(VicsekModel.distances(r, 2))==type(np.array(1)))
+    assert(type(VicsekModel.distances(r, 2)) is type(np.array(1)))
+    #assert(isinstance(VicsekModel.distances(r, 2), type(np.array(1))))
